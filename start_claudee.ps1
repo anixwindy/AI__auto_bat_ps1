b6f7aed5017a0b63cd29f71f8d955163c7aba042
Jenny100901 <196703104+Jenny100901@users.noreply.github.com>
2026-05-12 04:11:12 +0800

    Add files via upload

--- start_claudee.ps1
+++ start_claudee.ps1
@@ -4,13 +4,12 @@ Write-Host ""
 Write-Host ""
 Write-Host "進入 Claude後可以輸入："
 Write-Host ""
-Write-Host "Clear-Content "$env:USERPROFILE\.codex\history.jsonl""
-Write-Host ""
-Write-Host "Get-Item "$env:USERPROFILE\.codex\history.jsonl" | Select-Object Name, Length"
+
 Write-Host ""
 Write-Host ""
 
 
 
 
-claude --permission-mode default --add-dir D:\Cthis
+claude --permission-mode default
+claude --add-dir D:\Cthis --disallowedTools "Bash"
