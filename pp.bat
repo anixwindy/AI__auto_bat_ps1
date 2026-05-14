aaf583d8dc26cc3b6e1cc660ce50b9e87a126616
Jenny100901 <196703104+Jenny100901@users.noreply.github.com>
2026-05-15 07:23:14 +0800

    Add files via upload

---
+++ pp.bat
@@ -0,0 +1,10 @@
+@echo off
+
+wt ^
+  new-tab ^
+  --title "Claude" ^
+  --suppressApplicationTitle ^
+powershell.exe -NoExit -NoProfile -ExecutionPolicy Bypass -File "D:\vs_vs\ol_py\start_claudddee.ps1"
+
+
+
